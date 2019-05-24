7da39a4897519817f4d7552d68811849d16aa073
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-05-24 11:00:01 +0200

    Bower, bootstrap y moment instalados

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/peliculas', usersRouter);
+app.use('/components', express.static(`${__dirname}/public/components`));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
